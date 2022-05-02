06466bb919750ce01b3df600a8fa69402fc897da
ukyang <912413803@qq.com>
2022-05-03 01:17:24 +0800

    first commit

---
+++ src/main/java/com/ukyang/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.ukyang.git;
+
+/**
+ * @Description
+ * @Author ukyang
+ * @Date 2022/5/3 0:55
+ * @Version v1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
