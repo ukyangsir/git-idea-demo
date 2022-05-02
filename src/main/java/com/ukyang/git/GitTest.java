06e08385c6f5652d9eaef0417b3c3fb4f907d4dd
ukyang <912413803@qq.com>
2022-05-03 01:37:10 +0800

    forth commit

--- src/main/java/com/ukyang/git/GitTest.java
+++ src/main/java/com/ukyang/git/GitTest.java
@@ -8,7 +8,7 @@ package com.ukyang.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git4");
         System.out.println("hello git2");
         System.out.println("hello git3");
     }
