68ea4745e6db1ea10a84e5270dded708796ea832
ukyang <912413803@qq.com>
2022-05-03 14:48:40 +0800

    nineth commit

--- src/main/java/com/ukyang/git/GitTest.java
+++ src/main/java/com/ukyang/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("hello git6");
+        System.out.println("hello git7");
     }
 }
